830cdb2581d76fd48203e224120cbfe2cf5af019
Brayan <brayanjcp@outlook.com>
2018-08-25 13:17:46 -0500

    Rutas de las apis

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'login/index'
   root 'login#index'
+
+  resources :user
+  resources :event
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
