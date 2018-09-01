d2d436f1f81a6d52a9ff0c121d451ae30c250a94
Brayan <brayanjcp@outlook.com>
2018-09-01 11:16:47 -0500

    ajuste de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       post 'event_create' => 'service#createEvent'
       post 'role_list' => 'service#getRoles'
       post 'user_get_user' => 'service#getUser'
+      post 'user_list' => 'service#getUsers'
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
